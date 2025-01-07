dfdf0aaabd19a6bb7881670fc5e2a70b51aa686a
matth <matthew.c.hirstius@gmail.com>
2025-01-06 19:47:58 -0600

    Initial Commit

---
+++ API.py
@@ -0,0 +1,78 @@
+# Using flask to make an api 
+# import necessary libraries and functions 
+from flask import Flask, jsonify, request, make_response 
+import flask_cors
+import DataCleaning
+import PredictionModel
+import datetime
+import pandas as pd
+import threading
+
+DataCleaning.UpdateDataFiles()
+
+washerModel = None
+dryerModel = None
+def Retrain():
+    global washerModel
+    global dryerModel
+    threading.Timer(86400.0, Retrain).start()
+    print("Retrained")
+    washerModel = PredictionModel.CreateModel("Washing Machines")
+    dryerModel = PredictionModel.CreateModel("Dryers")
+
+Retrain()
+# creating a Flask app 
+app = Flask(__name__)
+app.json.sort_keys = False
+cors = flask_cors.CORS(app) # allow CORS for all domains on all routes.
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+@app.route('/', methods = ['GET']) 
+def home():
+    data = "hello world"
+    return jsonify({'data': data}) 
+
+@app.route('/current/<int:hall>', methods = ['GET']) 
+def current(hall):
+    return jsonify({'Washing Machines': int(PredictionModel.GetCurrentPrediction(washerModel, hall)),
+                    "Dryers": int(PredictionModel.GetCurrentPrediction(dryerModel, hall))}) 
+
+@app.route('/currentTime', methods = ['GET']) 
+def getTime():
+    return jsonify({'Time': PredictionModel.getLabel()}) 
+  
+@app.route('/today/<int:hall>', methods = ['GET']) 
+def today(hall):
+    return jsonify({'Washing Machines': PredictionModel.GetWholeDayPrediction(washerModel, hall, datetime.datetime.now()),
+                    "Dryers": PredictionModel.GetWholeDayPrediction(dryerModel, hall, datetime.datetime.now())}) 
+
+@app.route('/day/<int:hall>/dayOfMonth/<int:dayOfMonth>/month/<int:month>', methods = ['GET']) 
+def day(hall, dayOfMonth, month):
+    return jsonify({'Washing Machines': PredictionModel.GetWholeDayPrediction(washerModel, hall, datetime.datetime(datetime.datetime.now().year, month, dayOfMonth)),
+                    "Dryers": PredictionModel.GetWholeDayPrediction(dryerModel, hall, datetime.datetime(datetime.datetime.now().year, month, dayOfMonth))}) 
+  
+@app.route('/week/<int:hall>', methods = ['GET'])
+def week(hall):
+    return jsonify({'Current Time': PredictionModel.getLabel(), 'Washing Machines': PredictionModel.GetWholeWeekPrediction(washerModel, hall),
+                    "Dryers": PredictionModel.GetWholeWeekPrediction(dryerModel, hall)})
+@app.route('/optimumTime/<int:hall>/startDayOfMonth/<int:startDayOfMonth>/startMonth/<int:startMonth>/endDayOfMonth/<int:endDayOfMonth>/endMonth/<int:endMonth>/step/<int:step>', methods = ['GET']) 
+def optimumTime(hall, startDayOfMonth, startMonth, endDayOfMonth, endMonth, step):
+    return jsonify({"Optimum Time": PredictionModel.GetOptimumTime(washerModel, dryerModel, hall, datetime.datetime(datetime.datetime.now().year, startMonth, startDayOfMonth), datetime.datetime(datetime.datetime.now().year, endMonth, endDayOfMonth), step)})
+@app.route('/contribute', methods = ['POST']) 
+def contribute():
+    try:
+        data = request.json['data']
+        df = pd.read_csv("Laundry Data Pro\\laundry-API-backend\\Data Files\\WebAppData.csv")
+        if (((df['How many Washing Machines are Available?'] == data[0]) & (df['How many Dryers are Available?'] == data[1]) & (df['What Hall?'] == data[2]) & (df['Month'] == data[3]) & (df['Weekday'] == data[4]) & (df['Hour'] == data[5])).any()):
+            return make_response("POST request contains duplicate data", 202)
+        df.loc[df["What Hall?"].size] = request.json['data']
+        df.to_csv("Laundry Data Pro\\laundry-API-backend\\Data Files\\WebAppData.csv", index=False)
+        return make_response("POST request succeded", 200)
+    except:
+        return make_response("POST request failed", 201)
+  
+
+# driver function 
+if __name__ == '__main__': 
+  
+    app.run(debug = True) 
---
+++ PredictionModel.py
@@ -0,0 +1,116 @@
+import datetime
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+
+dayOfWeekDict = {
+    6: "Sunday",
+    0: "Monday",
+    1: "Tuesday",
+    2: "Wednesday",
+    3: "Thursday",
+    4: "Friday",
+    5: "Saturday"
+}
+
+def CreateModel(machineType: str):
+    df = pd.concat([pd.read_csv("Laundry Data Pro\\Laundry Data Pro\\backend\Data Files\\GoogleFormData.csv"), pd.read_csv("Laundry Data Pro\\Laundry Data Pro\\backend\\Data Files\\WebAppData.csv")], axis=0)
+
+    #Assigns Independent and Dependent variables in the form of X and y
+    X = df.drop(["How many Washing Machines are Available?", "How many Dryers are Available?"], axis = 1)
+    y = df["How many " + machineType + " are Available?"]
+
+    #Creates testing data sets and training data sets with training making up 80% of the origninal dataset and testing being 20%
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2)
+
+    #Defines and Fits each of the respective classifiers
+    dtr = RandomForestRegressor(max_depth=10, n_estimators=200)
+    dtr.fit(X_train, y_train)
+    return dtr
+
+def GetCurrentPrediction(model: RandomForestRegressor, hall: str):
+    day = datetime.datetime.now()
+    df = pd.DataFrame(columns=["What Hall?", "Month", "Weekday", "Hour"])
+    df['What Hall?'] = [hall]
+    df['Month'] = [day.month]
+    df['Weekday'] = [day.weekday()]
+    df['Hour'] = [day.hour]
+    return round(model.predict(df)[0])
+
+def GetWholeDayPrediction(model: RandomForestRegressor, hall: str, day: datetime):
+    predictions = {}
+    df = pd.DataFrame(columns=["What Hall?", "Month", "Weekday", "Hour"])
+    df['What Hall?'] = [hall]
+    df['Month'] = [day.month]
+    df['Weekday'] = [day.weekday()]
+    low = 100
+    high = -100
+    for i in range (24):
+        df['Hour'] = [i]
+        predInt = round(model.predict(df)[0])
+        if (predInt > high):
+                high = predInt
+                highIndex = f"{format_hour(i)}"
+        if (predInt < low):
+                low = predInt
+                lowIndex = f"{format_hour(i)}"
+        predictions[i] = predInt
+    return {"Predictions": predictions, "Low": lowIndex, "High": highIndex}
+
+def GetOptimumTime(washers: RandomForestRegressor, dryers: RandomForestRegressor, hall: str, startDay: datetime, endDay: datetime, step: int):
+    timeArr = []
+    df = pd.DataFrame(columns=["What Hall?", "Month", "Weekday", "Hour"])
+    iterDate: datetime = startDay
+    while (iterDate < endDay):
+        df['What Hall?'] = [hall]
+        df['Month'] = [iterDate.month]
+        df['Weekday'] = [iterDate.weekday()]
+        timeArr.append({'time': iterDate,"bestTime": GetOptimumTimeDay(washers, dryers, df)})
+        iterDate += datetime.timedelta(days=step)
+    return timeArr
+
+def GetOptimumTimeDay(washers, dryers, df):
+    bestTime = 0
+    for i in range (24):
+        df['Hour'] = [i]
+        avgPredInt = (washers.predict(df)[0] + dryers.predict(df)[0])/2
+        if (avgPredInt > bestTime):
+                bestTime = avgPredInt
+                bestTimeIndex = f"{format_hour(i)}"
+    return bestTimeIndex
+
+def GetWholeWeekPrediction(model: RandomForestRegressor, hall: str):
+    predictions = {}
+    day = datetime.datetime.now()
+    low = 100
+    lowIndex = ""
+    highIndex = ""
+    high = -100
+    for i in range (7):
+        newDay = (day + datetime.timedelta(days=i))
+        df = pd.DataFrame(columns=["What Hall?", "Month", "Weekday", "Hour"])
+        df['What Hall?'] = [hall]
+        df['Month'] = [newDay.month]
+        df['Weekday'] = [newDay.weekday()]
+        for x in range (24):
+            df['Hour'] = [x]
+            predInt = round(model.predict(df)[0])
+            if (predInt > high):
+                high = predInt
+                highIndex = f"{dayOfWeekDict[newDay.weekday()]} {format_hour(x)}"
+            if(predInt < low):
+                low = predInt
+                lowIndex = f"{dayOfWeekDict[newDay.weekday()]} {format_hour(x)}"
+            predictions[f"{dayOfWeekDict[newDay.weekday()]} {format_hour(x)}"] = predInt
+    predictions["High"] = highIndex
+    predictions['Low'] = lowIndex
+    return predictions
+
+def format_hour(hour):
+    hour_int = int(hour)  # Ensure hour is an integer
+    period = "am" if hour_int < 12 else "pm"
+    hour_formatted = 12 if hour_int % 12 == 0 else hour_int % 12  # Convert to 12-hour format
+    return f"{hour_formatted}:00{period}".upper()
+
+def getLabel():
+    return f"{dayOfWeekDict[datetime.datetime.now().weekday()]} {format_hour(datetime.datetime.now().hour)}"
