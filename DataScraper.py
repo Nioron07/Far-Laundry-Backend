e2c7cc256f31e85d72f67643747aff35dd97d789
matth <matthew.c.hirstius@gmail.com>
2025-01-22 10:25:35 -0600

    options test

--- DataScraper.py
+++ DataScraper.py
@@ -12,13 +12,14 @@ chrome_options = Options()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument( "--remote-debugging-pipe" )
     
 # Initialize the WebDriver (Chrome in this example)
 # Make sure you've installed ChromeDriver and have it in your PATH.
 
-driver = webdriver.Chrome(options=chrome_options)
 tz = pytz.timezone("US/Central")
 def scrape_laundry_summary(db: sqlalchemy.engine.base.Engine):
+    driver = webdriver.Chrome(options=chrome_options)
     Og_url = "https://mycscgo.com/laundry/summary/b94db20b-3bf8-4517-9cae-da46d7dd73f6/2303113-025"
     Tr_url = "https://mycscgo.com/laundry/summary/b94db20b-3bf8-4517-9cae-da46d7dd73f6/2303113-026"
     
@@ -62,6 +63,7 @@ def scrape_laundry_summary(db: sqlalchemy.engine.base.Engine):
             print(f"Unable to find element 2: {e}")
     
     finally:
+        driver.quit()
         print("starting SQL")
         now = datetime.now(tz=tz)
         stmt = sqlalchemy.text(
